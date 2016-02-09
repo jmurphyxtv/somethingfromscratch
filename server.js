2e23f3b22dd125158f2ca80fa36c1d552080ee1b
John Murphy <john.murphy@xtv.net>
2016-02-08 18:42:28 -0800

    basic isomorphic js website with socket.io managing future page changes

--- server.js
+++ server.js
@@ -17,8 +17,93 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get(['/','/about','/contact'], function(req,res,next) {
-  res.sendFile(__dirname + '/public/index.html');
+var routes = [
+  {
+    paths: ['/', '/home', '/index'],
+    mapTo: 'home'
+  },
+  {
+    paths: ['/about'],
+    mapTo: 'about'
+  },
+  {
+    paths: ['/contact'],
+    mapTo: 'contact'
+  }
+];
+
+var loadAsset = function(file, cb) {
+  console.log('asset');
+  fs.readFile(__dirname + file, 'utf8', function(err, response) {
+    if (err) {
+      console.error(err);
+      cb('');
+    } else {
+      cb(response);
+    }
+  });
+};
+
+var layoutHTML;
+function getLayoutHTML() {
+  loadAsset('/public/common/layout.html', function(page) {
+    layoutHTML = page;
+  });
+}
+getLayoutHTML();
+
+var getMapToFromPath = function(path) {
+  for (var i = 0; i < routes.length; i++) {
+    var route = routes[i];
+    if (route.paths.indexOf(path) !== -1) {
+      return routes[i].mapTo;
+    }
+  }
+  return null;
+};
+
+var getPage = function(page, cb) {
+  loadAsset('/public/' + page + '/' + page + '.js', function(js) {
+    loadAsset('/public/' + page + '/' + page + '.html', function(html) {
+      cb({
+        title: page,
+        js: js,
+        css: css,
+        html: html
+      });
+    });
+  });
+};
+
+var getPageAndSendResponse = function(page, res) {
+  getPage(page, function(data) {
+    var resHTML = layoutHTML.replace('{john-bindJS}', data.js).replace('{john-bindHTML}', data.html);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(resHTML);
+    res.end();
+  });
+};
+
+routes.forEach(function(route, i) {
+  app.get(route.paths, function(req, res, next) {
+    var mapTo = getMapToFromPath(req.url);
+    getPageAndSendResponse(mapTo, res);
+  });
 });
 
+console.log('hereeee')
+
+
 app.use(express.static(__dirname + '/public'));
+app.use('*', function(req, res, next) {
+  console.log('easd');
+    getPageAndSendResponse('404', res);
+});
+
+io.on('connection', function(socket) {
+  socket.on('getPage', function(pageRequested) {
+    getPage(pageRequested.page, function(pageData) {
+      socket.emit('pageData', pageData);
+    });
+  });
+});
