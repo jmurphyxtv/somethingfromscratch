63a7b12dfe5ef2ca1857b3fec574a5554da022b6
John Murphy <john.murphy@xtv.net>
2016-02-13 16:11:48 -0800

    moreaws

--- server.js
+++ server.js
@@ -32,9 +32,9 @@ var S3_BUCKET;
 //   }
 // });
 
-AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
-AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
-S3_BUCKET = process.env.S3_BUCKET;
+AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY_ID;
+AWS_SECRET_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+S3_BUCKET = process.env.S3_BUCKET_NAME;
 console.log(AWS_ACCESS_KEY,AWS_SECRET_KEY,S3_BUCKET);
 
 var port = process.env.PORT || 5000; // Use the port that Heroku
